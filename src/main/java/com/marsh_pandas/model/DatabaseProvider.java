d5dbd8efce3eb9d9fb8473e91e7f49acb0b9750b
Artur Kasymov <art.kasymov@gmail.com>
2020-02-29 15:33:19 +0100

    psql

--- src/main/java/com/marsh_pandas/model/DatabaseProvider.java
+++ src/main/java/com/marsh_pandas/model/DatabaseProvider.java
@@ -10,19 +10,11 @@ public class DatabaseProvider{
     private Connection connection;
 
     public DatabaseProvider() throws Exception{
-
-
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
         connection = DriverManager.getConnection(dbUrl, username, password);
-
-
-        ResultSet rs = connection.prepareStatement("SELECT NOW();").executeQuery();
-        System.out.println(rs.getDate(1).toString());
     }
 
 
