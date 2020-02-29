6bb0ca143bb15c68c63fd10a26da181a496fbb39
Artur Kasymov <art.kasymov@gmail.com>
2020-02-29 15:18:40 +0100

    postgresql

--- src/main/java/com/marsh_pandas/model/DatabaseProvider.java
+++ src/main/java/com/marsh_pandas/model/DatabaseProvider.java
@@ -1,6 +1,19 @@
 package com.marsh_pandas.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
 public class DatabaseProvider{
+
+    private Connection connection;
+
+    public DatabaseProvider() throws Exception{
+        connection = DriverManager.getConnection(System.getenv("JDBC_DATABASE_URL"));
+
+        ResultSet rs = connection.prepareStatement("SELECT NOW();").executeQuery();
+        System.out.println(rs.getDate(1).toString());
+    }
 
 
 
